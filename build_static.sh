b4619cd52bf258a085c75a3ecf4d2d7a53c834a2
Mathias Robert <mathias-robert@hotmail.fr>
2026-01-16 20:53:26 +0100

    Fix build

--- build_static.sh
+++ build_static.sh
@@ -1,9 +1,10 @@
 git clone -b master git@github.com:MathiasRobert/birdex.git deps/birdex
+cp .env.birdex ./deps/birdex/.env
 cd ./deps/birdex/
 git pull
 
 bun install
-bun run build
+bun run build --base=/projects/birdex
 cd ../../
 rm -rf projects/birdex
 cp -R deps/birdex/dist projects/birdex
